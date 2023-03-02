92a4f6da62cca58e6b98c6d43d503e54d16ff314
niteshduraivel <niteshduraivel@tamu.edu>
2023-03-01 19:29:37 -0600

    nitesh2

--- SQL/moreInventory.sql
+++ SQL/moreInventory.sql
@@ -4,6 +4,7 @@ VALUES
 	(DEFAULT, 500, 1.09, 'Dark Chocolate Cashews'),
 	(DEFAULT, 500, 1.59, 'Greek Yogurt Pretzels'),
 	(DEFAULT, 500, 1.09, 'Ostrim Meat Stick'),
-	(DEFAULT, 500, 1.49, 'Quest Protein Bar')
+	(DEFAULT, 500, 1.49, 'Quest Protein Bar'),
+	(DEFAULT, 1000, 2.51, 'Strawberry')
 	;
 	
