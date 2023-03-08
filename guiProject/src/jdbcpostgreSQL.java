0f297870f193ceaa71ea08b9f0dd38f2212a992c
tfella <tylerfelton@tamu.edu>
2023-03-07 19:42:48 -0600

    EMPLOYEE VIEW WORKS

--- guiProject/src/jdbcpostgreSQL.java
+++ guiProject/src/jdbcpostgreSQL.java
@@ -1,14 +1,32 @@
 import java.sql.*;
-import javax.swing.*; 
+import java.text.DecimalFormat;
+
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.lang.Math.*;
 
 /*
 CSCE 315
 9-27-2021 Lab
  */
 
-public class jdbcpostgreSQL extends JFrame {
+public class jdbcpostgreSQL extends JFrame implements ActionListener {
     static JFrame f;
+    String orderList = "";
+    String orderListID = "";
+    Connection conn = null;
+    String teamNumber = "team_73";
+    String dbName = "csce315331_" + teamNumber;
+    String dbConnectionString = "jdbc:postgresql://csce-315-db.engr.tamu.edu/" + dbName;
+    JTextArea orderArea = new JTextArea();
+    JTextField text = new JTextField();
+    double orderTotal;
+    int employeeID;
+    int currOrderId;
 
     // Commands to run this script
     // This will compile all java files in this directory
@@ -19,8 +37,7 @@ public class jdbcpostgreSQL extends JFrame {
     // Mac/Linux: java -cp ".:postgresql-42.2.8.jar" jdbcpostgreSQL
 
     // MAKE SURE YOU ARE ON VPN or TAMU WIFI TO ACCESS DATABASE
-    public static void main(String args[]) {
-
+    public jdbcpostgreSQL() {
         // Building the connection with your credentials
         Connection conn = null;
         String teamNumber = "team_73";
@@ -35,73 +52,76 @@ public class jdbcpostgreSQL extends JFrame {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
+        
 
+        try {
+            Statement newStmt = conn.createStatement();
+            String sqlStatement = "SELECT MAX(orderid) FROM orders";
+            ResultSet result = newStmt.executeQuery(sqlStatement);
+            if (result.next()) {
+                currOrderId = result.getInt("max") + 1 ; 
+            }        
+        } catch (Exception d) {
+            d.printStackTrace();
+            System.err.println(d.getClass().getName() + ": " + d.getMessage());
+            System.exit(0);
+        }
+        
         System.out.println("Opened database successfully");
         JPanel buttonPanel = new JPanel(new GridLayout(20, 3));
         buttonPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-        
-        String name = "";
+        buttonPanel.setPreferredSize(new Dimension(300, 300));
+
         try {
             // create a statement object
             Statement stmt = conn.createStatement();
-
-            // Running a query
-            // TODO: update the sql command here
             String sqlStatement = "SELECT * FROM menu";
-
-            // send statement to DBMS
-            // This executeQuery command is useful for data retrieval
             ResultSet result = stmt.executeQuery(sqlStatement);
-            // OR
-            // This executeUpdate command is useful for updating data
-            // int result = stmt.executeUpdate(sqlStatement);
 
-            // OUTPUT
-            // You will need to output the results differently depeninding on which function
-            // you use
-
-            System.out.println("--------------------Query Results--------------------");
             while (result.next()) {
-                name += result.getString("itemPrice") + "\n"; // Replace column_name with any column name on your
-                                                              // table
+
                 JButton button = new JButton();
 
                 // Set the button's text and properties
                 button.setText(result.getString("itemname"));
                 button.setName(result.getString("itemname"));
-                button.setSize(100, 50);
-
-                // Add the button to the panel
+                button.addActionListener(this);
                 buttonPanel.add(button);
+
             }
-
-            // create a new frame
-            // create a new frame
             f = new JFrame("DB GUI");
-
-            // create a object
-            
-            // create a panel
-            JPanel p = new JPanel();
-            p.setSize(400, 400);
-          
-
-            
-
-            // TODO Step 3
-           
-            
-            // TODO Step 4
-
-          
-            p.add(buttonPanel);
+            JSplitPane p = new JSplitPane();
+            p.setDividerLocation(0.5);
+            p.setLeftComponent(buttonPanel);
             // add panel to frame
-            f.add(p);
-            
-            // set the size of frame
-            f.setSize(400, 400);
 
-            f.show();
+            orderArea.setBorder(BorderFactory.createLineBorder(Color.black));
+            orderArea.setPreferredSize(new Dimension(300, 300));
+            JPanel lowPanel = new JPanel();
+            lowPanel.add(orderArea);
+            JButton orderButton = new JButton();
+            // Set the button's text and properties
+            orderButton.setText("Place Order");
+            orderButton.setName("Place Order");
+            orderButton.addActionListener(this);
+
+            text.setText("Enter Employee ID");
+
+            lowPanel.add(text);
+            lowPanel.add(orderButton);
+
+            p.setRightComponent(lowPanel);
+            p.setDividerLocation(0.75);
+
+            f.add(p);
+            // JPanel orderPanel = new JPanel();
+            // orderPanel.add(orderButton);
+            // f.add(orderPanel);
+
+            // set the size of frame
+            f.setSize(800, 800);
+
+            f.setVisible(true);
             // OR
             // System.out.println(result);
         } catch (Exception e) {
@@ -110,14 +130,83 @@ public class jdbcpostgreSQL extends JFrame {
             System.exit(0);
         }
 
-        // closing the connection
-        try {
-            conn.close();
-            System.out.println("Connection Closed.");
-        } catch (Exception e) {
-            System.out.println("Connection NOT Closed.");
-        }
+    }
 
-        // end try catch
+    public void actionPerformed(ActionEvent e) {
+        // Update global variable when a button is clicked
+        JButton o = (JButton) e.getSource();
+        String name = o.getName();
+        // Connecting to the database
+        if (name == "Place Order") {
+            // add order here
+            employeeID = Integer.parseInt(text.getText());
+            double totalOrder = orderTotal * 1.0825;
+            DecimalFormat df = new DecimalFormat("#.##"); // round to 2 decimal places
+            double roundedNum = Double.parseDouble(df.format(totalOrder));
+
+            Random rand = new Random();
+            int customerID = rand.nextInt(2000) + 1;
+            LocalDateTime currentTime = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            String currTime = currentTime.format(formatter);
+            orderListID = orderListID.substring(0, orderListID.length() - 2);
+            try {
+                conn = DriverManager.getConnection(dbConnectionString, dbSetup.user, dbSetup.pswd);
+            } catch (Exception c) {
+                c.printStackTrace();
+                System.err.println(c.getClass().getName() + ": " + c.getMessage());
+                System.exit(0);
+            }
+            try {
+                Statement newStmt = conn.createStatement();
+                String sqlStatement = "INSERT INTO orders VALUES (" + currOrderId + ", '" + currTime + "', " + employeeID + ", " +customerID + ", '" + orderListID + "', " + roundedNum + ")";
+                newStmt.executeUpdate(sqlStatement); 
+            } catch (Exception d) {
+                d.printStackTrace();
+                System.err.println(e.getClass().getName() + ": " + d.getMessage());
+                System.exit(0);
+            }
+
+            // reset variables
+            orderList = "";
+            orderListID = "";
+            orderTotal = 0.0;
+            text.setText("Please Enter EmployeeID");
+            orderArea.setText("");
+            currOrderId += 1;
+        } else {
+            try {
+                conn = DriverManager.getConnection(dbConnectionString, dbSetup.user, dbSetup.pswd);
+            } catch (Exception c) {
+                c.printStackTrace();
+                System.err.println(c.getClass().getName() + ": " + c.getMessage());
+                System.exit(0);
+            }
+
+            try {
+                Statement newStmt = conn.createStatement();
+                String sqlStatement = "SELECT menuid, itemprice FROM menu WHERE itemName = '" + name + "'";
+                ResultSet result = newStmt.executeQuery(sqlStatement);
+                if (result.next()) {
+                    orderListID += result.getInt("menuid") + ", ";
+                    orderTotal += result.getDouble("itemprice");
+                }
+                System.out.println(orderListID);
+                System.out.println(orderListID.substring(0, orderListID.length() - 2));
+            } catch (Exception d) {
+                d.printStackTrace();
+                System.err.println(e.getClass().getName() + ": " + d.getMessage());
+                System.exit(0);
+            }
+
+            orderList += name + ", \n";
+            orderArea.setText(orderList);
+        }
+    }
+
+    public static void main(String args[]) {
+
+        new jdbcpostgreSQL();
+
     }// end main
 }// end Class
